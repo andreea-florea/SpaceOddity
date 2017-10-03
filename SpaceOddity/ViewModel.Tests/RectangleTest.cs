135cd574820f18e6672105b67bbabfd214652404
tenerifeeHub <moldoveanuFlorin1@yahoo.com>
2017-10-03 22:55:38 +0300

    Created View model project

---
+++ SpaceOddity/ViewModel.Tests/RectangleTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViewModel.Interfaces;
+
+namespace ViewModel.Tests
+{
+    [TestClass]
+    public class RectangleTest
+    {
+    }
+}
---
+++ SpaceOddity/ViewModel/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Interfaces;
+
+namespace ViewModel
+{
+    public class Rectangle : IRectangle
+    {
+        public double Width
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public double Height
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
