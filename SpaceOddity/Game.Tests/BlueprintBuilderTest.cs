e2d770059a6ec5e075bc1148f8add022dc251501
Andreea Florea <andreeacorisecelean@gmail.com>
2017-10-03 16:42:16 +0300

    modified BlueprintBuilder tests to use Moq

--- SpaceOddity/Game.Tests/BlueprintBuilderTest.cs
+++ SpaceOddity/Game.Tests/BlueprintBuilderTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Game.Tests.Mocks;
+using Moq;
 
 namespace Game.Tests
 {
@@ -9,21 +10,22 @@ namespace Game.Tests
     {
         private IBlock[,] blueprint;
         private BlueprintBuilder blueprintBuilder;
-        private MockBlockFactory mockBlockFactory;
+        private Mock<IBlockFactory> mockBlockFactory;
+        private Mock<IBlock> mockBlock;
 
         [TestInitialize]
         public void Init()
         {
             blueprint = new IBlock[9, 10];
-            mockBlockFactory = new MockBlockFactory(new MockBlock());
-            blueprintBuilder = new BlueprintBuilder(blueprint, mockBlockFactory);
+            mockBlock = new Mock<IBlock>();
+            mockBlockFactory = new Mock<IBlockFactory>();
+            blueprintBuilder = new BlueprintBuilder(blueprint, mockBlockFactory.Object);
         }
 
         [TestMethod]
         public void CheckIfBlueprintMatrixIsAssignedCorrectly()
         {
             blueprint[2, 3] = new MockBlock();
-
             Assert.AreEqual(9, blueprintBuilder.Height);
             Assert.AreEqual(10, blueprintBuilder.Width);
             Assert.AreEqual(blueprint[2, 3], blueprintBuilder.GetBlock(2, 3));
@@ -32,28 +34,30 @@ namespace Game.Tests
         [TestMethod]
         public void CheckIfBlockGetsCreatedCorrectlyOnEmptySpot()
         {
-            Assert.IsTrue(blueprintBuilder.CreateBlock(4, 5));
+            blueprint[4, 5] = new Mock<IBlock>().Object;
             Assert.AreNotEqual(blueprint[4, 5], null);
         }
 
         [TestMethod]
         public void BlockCantBeCreatedIfSpotOccupied()
         {
-            Assert.IsTrue(blueprintBuilder.CreateBlock(4, 5));
+            blueprint[4, 5] = new Mock<IBlock>().Object;
             Assert.IsFalse(blueprintBuilder.CreateBlock(4, 5));
         }
 
         [TestMethod]
         public void CheckIfBlockFactoryIsUsedToCreateOtherBlocks()
         {
+            MockBlock mockBlock = new MockBlock();
+            mockBlockFactory.Setup(x => x.CreateBlock()).Returns(mockBlock);
             Assert.IsTrue(blueprintBuilder.CreateBlock(4, 5));
-            Assert.AreEqual(mockBlockFactory.Block, blueprintBuilder.GetBlock(4, 5));
+            Assert.AreEqual(mockBlock, blueprintBuilder.GetBlock(4, 5));
         }
 
         [TestMethod]
         public void CheckIfExistentBlockIsDeletedSuccessfully()
         {
-            Assert.IsTrue(blueprintBuilder.CreateBlock(4, 5));
+            blueprint[4, 5] = new Mock<IBlock>().Object;
             Assert.AreNotEqual(null, blueprint[4, 5]);
             Assert.IsTrue(blueprintBuilder.DeleteBlock(4, 5));
             Assert.AreEqual(null, blueprint[4, 5]);
