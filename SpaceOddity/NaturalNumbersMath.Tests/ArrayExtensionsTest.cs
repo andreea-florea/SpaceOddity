de0e28c9a088fac687b7ed1604f885618e8c54fb
tenerifeeHub <moldoveanuFlorin1@yahoo.com>
2017-10-28 00:36:05 +0300

    Added forgoten unit test for array extension method

---
+++ SpaceOddity/NaturalNumbersMath.Tests/ArrayExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NaturalNumbersMath.Tests
+{
+    [TestClass]
+    public class ArrayExtensionsTest
+    {
+        [TestMethod]
+        public void CanAccessMemberOfArrayWithExtension()
+        {
+            int[,] array = new int[3, 4];
+            array[2, 1] = 5;
+            Assert.AreEqual(5, array.Get(new Coordinate(1, 2)));
+        }
+    }
+}
