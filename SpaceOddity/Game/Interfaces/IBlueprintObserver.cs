2bb01a8116d7d18d8048eb23c9819853d859887c
Andreea Florea <andreeacorisecelean@gmail.com>
2017-12-13 17:54:30 +0200

    no message

---
+++ SpaceOddity/Game/Interfaces/IBlueprintObserver.cs
@@ -0,0 +1,20 @@
+﻿using NaturalNumbersMath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game.Interfaces
+{
+    public interface IBlueprintObserver
+    {
+        void BlockCreated(IBlueprint blueprintBuilder, Coordinate position);
+        void BlockDeleted(IBlueprint blueprintBuilder, Coordinate position);
+        void ShipComponentAdded(IBlueprint blueprintBuilder, Coordinate position);
+        void ShipComponentDeleted(IBlueprint blueprintBuilder, Coordinate position);
+        void DoubleEdgePipeAdded(IBlueprint blueprintBuilder, Coordinate position);
+        void DoubleEdgePipeDeleted(IBlueprint blueprintBuilder, Coordinate position);
+        void ConnectingPipeAdded(IBlueprint blueprintBuilder, Coordinate position);
+        void ConnectingPipeDeleted(IBlueprint blueprintBuilder, Coordinate position);
+    }
+}
