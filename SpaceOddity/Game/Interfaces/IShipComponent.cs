5459d3839ddecf676ab7810c2ee6c3773333c3cc
Andreea Florea <andreeacorisecelean@gmail.com>
2018-01-22 18:24:57 +0200

    no message

--- SpaceOddity/Game/Interfaces/IShipComponent.cs
+++ SpaceOddity/Game/Interfaces/IShipComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Game.Enums;
+using NaturalNumbersMath;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,9 @@ namespace Game.Interfaces
     public interface IShipComponent
     {
         BlueprintShipComponentType Type { get; }
+
         void AdditionalSetups(IBlueprintBuilder blueprintBuilder);
         void RemoveAdditionalSetups(IBlueprintBuilder blueprintBuilder);
+        bool CanBePlaced(IBlueprint blueprint, Coordinate position);
     }
 }
